1327f0b2ea004839a8bdcf6b563587dd773052a6
wilkinsonbrian <wilkibrian@gmail.com>
2022-05-09 08:09:54 -0400

    Initial commit of Wordle starter files

---
+++ Wordle.java
@@ -0,0 +1,34 @@
+/*
+ * File: Wordle.java
+ * -----------------
+ * This module is the starter file for the Wordle assignment.
+ * BE SURE TO UPDATE THIS COMMENT WHEN YOU COMPLETE THE CODE.
+ */
+
+public class Wordle {
+
+    public void run() {
+        gw = new WordleGWindow();
+        gw.addEnterListener((s) -> enterAction(s));
+    }
+
+/*
+ * Called when the user hits the RETURN key or clicks the ENTER button,
+ * passing in the string of characters on the current row.
+ */
+
+    public void enterAction(String s) {
+        gw.showMessage("You have to implement this method.");
+    }
+
+/* Startup code */
+
+    public static void main(String[] args) {
+        new Wordle().run();
+    }
+
+/* Private instance variables */
+
+    private WordleGWindow gw;
+
+}
